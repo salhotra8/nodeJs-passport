9fd2577dd21da86c0d383866330ca9336b0aadc3
salhotra8 <salhotra8@gmail.com>
2025-04-20 19:39:10 +0530

    update remote db details

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,12 @@ import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "newuser",
-  password: process.env.DB_PASSWORD,
-  database: "firstdatabase",
+  url:process.env.REMOTE_DB_URL,
+  host: "mysql-3bf6cbf1-f5url-ca43.j.aivencloud.com",
+  port: 26589,
+  username: "avnadmin",
+  password: process.env.REMOTE_DB_PASSWORD,
+  database: "remoteDatabase",
   synchronize: true,
   logging: false,
   entities: [User],
